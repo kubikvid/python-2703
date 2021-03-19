f8fe934b3bb58ad3341a9ed02d38575118a81a96
Chernykh Andrey <moonquiz@ya.ru>
2021-03-19 21:20:34 +0700

    code-2

--- example.py
+++ example.py
@@ -1,16 +1,18 @@
 # coding: utf-8
 from typing import *
 
+from models import Person
 
-def get_persons(numbers: Iterable[int]) -> List[int]:
-    return list(
+
+def get_persons(numbers: Iterable[int]) -> Tuple[int]:
+    return tuple(
         Person.objects.filter(
             number__in=numbers
         ).values_list('id', flat=True)
     )
 
 
-def intersect(ids_from_db: List[int], l2: List[int]) -> List[int]:
+def intersect(ids_from_db: Tuple[int], l2: List[int]) -> List[int]:
     result = list()
     for i in ids_from_db:
         if i in l2:
